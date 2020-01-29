814a83084fea51fd348993328ebc2d1a7bb6d051
Douglas Cardoso <douglas.dcm@gmail.com>
2020-01-28 23:18:51 -0300

    firsr commit

---
+++ WebAPIDemo/Controllers/TestsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestsController : ControllerBase
+    {
+        static List<string> strings = new List<string>
+        {
+            "value0", "value1", "value2"
+        };
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return strings;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return strings[id];
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            //add a value to the strings
+            strings.Add(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            strings[id] = value;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            strings.RemoveAt(id);
+        }
+    }
+}
